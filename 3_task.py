b9a544028fd91e236f6f5eff1adb8b54ad691746
Nadezhda Limanova <dindinka86@gmail.com>
2023-04-11 11:44:49 +0300

    commit changed

--- 3_task.py
+++ 3_task.py
@@ -2,17 +2,16 @@ import requests
 import datetime
 import time
 
-def get_quesions(from_date, to_date):
+
+def get_quesions(from_date, to_date, tag):
     fromdate = int(time.mktime(time.strptime(from_date, "%Y-%m-%d")))
     todate = int(time.mktime(time.strptime(to_date, "%Y-%m-%d")))
-    url = f'https://api.stackexchange.com/2.3/questions?{fromdate}&{todate}&order=desc&sort=activity&site=stackoverflow'
+    url = f'https://api.stackexchange.com//2.3/questions?{fromdate}&{todate}&order=desc&sort=activity&tagged={tag}&site=stackoverflow'
     resp = requests.get(url)
     response = resp.json()
-    questions = response.get('items')
-    for item in questions:
-      for k, v in item.items():
-          if k == 'tags' and 'python' in(item['tags']):
-            print(f'Вопросы, содержащие тег "python": {item["title"]}')
+    get_items = response.get('items')
+    for item in get_items:
+      questions = item.get('title')
+      print(questions)
 
-
-get_quesions("2023-4-9", "2023-4-11")
+get_quesions("2023-4-9", "2023-4-11", 'python')
